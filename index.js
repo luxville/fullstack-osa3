a83fb2887fbf19b94c4222ba492becd61ef310dd
Ville Luostarinen <ville.luostarinen@helsinki.fi>
2021-02-12 10:21:23 +0200

    uibuild

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const app = express()
 const mongoose = require('mongoose')
 const Person = require('./models/person')
 
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
 app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
 
 morgan.token('body', function(request, response) {
   if (request.body.name) {
@@ -20,55 +19,12 @@ morgan.token('body', function(request, response) {
   }
 })
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
-
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-  })
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Person = mongoose.model('Person', personSchema)
-
-/*let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]*/
 
 app.get('/info', (request, response) => {
   const date = new Date()
@@ -85,14 +41,17 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+  //const id = Number(request.params.id)
+  //const person = persons.find(person => person.id === id)
 
-  if (person) {
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
+  /*if (person) {
     response.json(person)
   } else {
     response.status(404).end()
-  }
+  }*/
   //console.log(person)
 })
 
@@ -105,7 +64,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  const names = persons.map(person => person.name)
+  //const names = persons.map(person => person.name)
 
   if (body.name === undefined) {
     return response.status(400).json({
@@ -122,7 +81,6 @@ app.post('/api/persons', (request, response) => {
   }
 
   const person = {
-    //id: generateId(),
     name: body.name,
     number: body.number
   }
@@ -130,13 +88,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-  //persons = persons.concat(person)
-  //console.log(person)
 })
-
-/*const generateId = () => {
-  return Math.floor(Math.random() * 1000000000)
-}*/
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
